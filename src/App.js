314f6370f24573c259a6be2f4d66823657497a10
adi1729 <adiyadav.1729@gmail.com>
2021-02-08 01:42:42 +0530

    React module 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import React, { Component } from 'react';
+import Header from './Header.js';
+import './App.css';
 
-function App() {
-  return (
-    <div >
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    let subscribers = [
+      {
+        name : "Aditya",
+        phone : "12345"
+      },
+      {
+        name : "Yadav",
+        phone : "2345"
+      }
+    ]
+    return (
+      <div>
+        <Header />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+          {
+            subscribers.map(sub => {
+              return <div className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                </div>
+            })
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
